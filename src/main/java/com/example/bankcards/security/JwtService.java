12ab4a598e2949720cf12802e358c13978c411aa
Peipo Konstantin <kostapejpo@gmail.com>
2026-07-04 23:17:35 +0300

    add auth service add http chain with jwt with user in spring security

---
+++ src/main/java/com/example/bankcards/security/JwtService.java
@@ -0,0 +1,40 @@
+package com.example.bankcards.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtService
+{
+    private final String secret = "secret-key";
+
+    private final long expirationMs = 1000 * 60 * 60;
+
+    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    public String generateToken(String username, List<String> roles)
+    {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims extractClaims(String token)
+    {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+}
---
+++ src/main/java/com/example/bankcards/service/auth/DefaultAuthService.java
@@ -0,0 +1,32 @@
+package com.example.bankcards.service.auth;
+
+import java.util.List;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultAuthService implements AuthService
+{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    @Override
+    public String login(String username, String password)
+    {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword()))
+        {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return jwtService.generateToken(user.getUsername(), List.of(user.getRole().getName().name()));
+    }
+}
